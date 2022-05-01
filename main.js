f84312c89ec19f67d1fd548178a027d8bf37760d
circuitvarious <83318536+circuitvarious@users.noreply.github.com>
2022-05-01 16:16:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+
+const hello = new Audio();
+hello.src = "Hello.mp3";
+
+const bye = new Audio();
+bye.src = "bye.mp3";
+
+const sit = new Audio();   
+sit.src = "sit.mp3";
+
+const standup = new Audio();
+standup.src = "stand-up.mp3";
+
+const lineup = new Audio();
+lineup.src = "Line up.mp3";
+
+const walk = new Audio();
+walk.src = "walk.mp3";
+
+const look = new Audio();
+look.src = "look.mp3";
+
+const getonschoolbus = new Audio();
+getonschoolbus.src = "get-on-the-school-bus.mp3";
+
+const raisehand = new Audio();
+raisehand.src = "raise-your-hand.mp3"
+
+const red = new Audio();
+red.src = "Red.mp3";
+
+const Orange = new Audio();
+Orange.src = "Orange .mp3";
+
+const Yellow = new Audio();
+Yellow.src = "Yellow.mp3";
+
+const blue = new Audio();
+blue.src = "Blue.mp3";
+
+const Green = new Audio();
+Green.src = "Green.mp3";
+
+const purple = new Audio();
+purple.src = "Purple .mp3";
+
+const bathroom = new Audio();
+bathroom.src = "Bathroom.mp3"
+
+const book = new Audio();
+book.src = "book.mp3";
+
+const carpet = new Audio();
+carpet.src = "carpet.mp3";
+
+const cleanup = new Audio();
+cleanup.src = "Clean up.mp3";
+
+const drink = new Audio();
+drink.src = "Drink.mp3";
+
+const eat = new Audio();
+eat.src = "Eat.mp3";
+
+const ineedhelp = new Audio();
+ineedhelp.src = "I want more help.mp3";
+
+const  howareyoufeeling = new Audio();
+howareyoufeeling.src = "How are you feeling today_.mp3";
+
+const pink = new Audio();
+pink.src = "Pink.mp3";
+
+const math = new Audio();
+math.src = "Math.mp3";
+
+const science = new Audio();
+science.src = "Science.mp3";
+
+const writing = new Audio();
+writing.src = "Write.mp3";
+
+const readinggroups = new Audio();
+readinggroups.src = "Reading Group.mp3";
+
+const tablegroups = new Audio();
+tablegroups.src = "Table Work.mp3";
+
+const reccess = new Audio();
+reccess.src = "Recess .mp3";
+
+const specials = new Audio();
+specials.src = "Specials .mp3";
+
+const listen = new Audio();
+listen.src = "Listen.mp3";
